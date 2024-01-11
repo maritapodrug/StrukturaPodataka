27f462e4408c194d10ad3044d6916d1f5119ab31
maritapodrug <147590762+maritapodrug@users.noreply.github.com>
2024-01-11 14:04:34 +0100

    Add files via upload

---
+++ Zad1/zad1.c
@@ -0,0 +1,107 @@
+/*1. Napisati program koji prvo proèita koliko redaka ima datoteka, tj. koliko ima studenata
+zapisanih u datoteci. Nakon toga potrebno je dinamièki alocirati prostor za niz struktura
+studenata (ime, prezime, bodovi) i uèitati iz datoteke sve zapise. Na ekran ispisati ime,
+prezime, apsolutni i relativni broj bodova.
+Napomena: Svaki redak datoteke sadrži ime i prezime studenta, te broj bodova na kolokviju.
+relatvan_br_bodova = br_bodova/max_br_bodova*100
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX_SIZE 128
+#define MAX_LINE 1024
+#define MAX_POINTS 15
+
+typedef struct student {
+	char name[MAX_SIZE];
+	char surname[MAX_SIZE];
+	double points;
+}student;
+
+double relativepoints(double abs)
+{
+	return ((double)abs / MAX_POINTS) * 100;
+}
+
+int rowcounter()
+{
+	FILE* fp = NULL;
+	char buffer[MAX_SIZE] = { 0 };
+	int count = 0;
+
+	fp = fopen("imena.txt", "r");
+
+	if (fp == 0)
+	{
+		printf("File not opened!\n");
+		return -1;
+	}
+
+	while (!feof(fp))
+	{
+		fgets(buffer, MAX_SIZE, fp);
+		count++;
+	}
+	fclose(fp);
+
+	return count;
+}
+
+student* memallocandreadstudents(int numrow) {
+	FILE* fp = NULL;
+	student* st = NULL;
+	int count = 0;
+
+	st = (student*)malloc(numrow * sizeof(student));
+
+	if (!st)
+	{
+		printf("Failed to allocate memory!");
+		return -1;
+	}
+
+	fp = fopen("imena.txt", "r");
+
+	if (fp == NULL)
+	{
+		printf("File not opened!");
+		return -1;
+	}
+
+	while (!feof(fp)) {
+		fscanf(fp, " %s %s %lf", st[count].name, st[count].surname, &st[count].points);
+		count++;
+	}
+
+	fclose(fp);
+
+	return st;
+}
+
+void printstudents(int number, student* st)
+{
+	int i;
+
+	printf("NAME\tSURNAME\tABS POINTS\tRELATIVE POINTS\n");
+
+	for (i = 0; i < number; i++)
+		printf("%s\t%s\t%lf\t%lf\n", st[i].name, st[i].surname, st[i].points, relativepoints(st[i].points));
+
+}
+
+int main()
+{
+	int count = rowcounter();
+	student* student = NULL;
+
+	if (count > 0) {
+		student = memallocandreadstudents(count);
+		printstudents(count, student);
+		free(student);
+	}
+
+	return 0;
+
+}
